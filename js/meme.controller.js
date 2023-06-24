5d1018639bc45244b8535e2c6daf79784350e9e1
Giora Marani <giora.marani@gmail.com>
2023-06-24 20:59:24 +0300

    small afects and designs

--- js/meme.controller.js
+++ js/meme.controller.js
@@ -18,7 +18,16 @@ var gImgs = [{ id: 1, url: "img/1.jpg", keywords: ['celebrity', 'wierd', 'all'] 
 { id: 6, url: "img/6.jpg", keywords: ['funny', 'movies', 'all'] },
 { id: 7, url: "img/7.jpg", keywords: ['kids', 'cute', 'all'] },
 { id: 8, url: "img/8.jpg", keywords: ['exciting', 'movies', 'all'] },
-{ id: 9, url: "img/9.jpg", keywords: ['exciting', 'movies', 'all'] }
+{ id: 9, url: "img/9.jpg", keywords: ['exciting', 'movies', 'all'] },
+{ id: 10, url: "img/10.jpg", keywords: ['politics', 'all'] },
+{ id: 11, url: "img/11.jpg", keywords: ['sports', 'violence', 'all'] },
+{ id: 12, url: "img/12.jpg", keywords:  ['movies', 'all'] },
+{ id: 13, url: "img/13.jpg", keywords: ['kids', 'funny', 'all'] },
+{ id: 14, url: "img/14.jpg", keywords: ['politics', 'all'] },
+{ id: 15, url: "img/15.jpg", keywords: ['movies', 'all'] },
+{ id: 16, url: "img/16.jpg", keywords: ['movies', 'funny' ,'all'] },
+{ id: 17, url: "img/17.jpg", keywords: ['politics', 'all'] },
+{ id: 18, url: "img/18.jpg", keywords: ['exciting', 'movies', 'funny' ,'all'] }
 ]
 var gSavedMemes = []
 
